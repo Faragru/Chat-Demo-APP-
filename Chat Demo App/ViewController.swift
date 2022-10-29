a9ea965196ee19e8870b433effd9533aacc7091e
Faragru <ciprians39@yahoo.com>
2022-10-30 00:15:48 +0300

    initial commit

---
+++ Chat Demo App/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  Chat Demo App
+//
+//  Created by Stan Ciprian on 19.10.2022.
+//
+
+import StreamChat
+import StreamChatUI
+import UIKit
+
+class DemoChannelList: ChatChannelListVC {}
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
